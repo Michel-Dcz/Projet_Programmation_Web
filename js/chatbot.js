5ce21296d16fc97dcc18c3003196b7ed81fc16f0
Albin255 <albin.ottone@efrei.net>
2026-04-15 11:33:55 +0200

    chat bot

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,5 +1,7 @@
 const chatBtn = document.getElementById('chat-button');
 const sidebar = document.getElementById('chat-sidebar');
+const input = document.querySelector('#user-input');
+const chatcontent = document.querySelector('.chat-content');
 
 chatBtn.addEventListener('click', () => {
     sidebar.classList.toggle('active');
@@ -12,3 +14,29 @@ document.addEventListener('keydown', (event) => {
         chatBtn.classList.remove('active');
     }
 });
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === "Enter" && sidebar.classList.contains('active')) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'userquestion');
+        var question = input.value;
+        let p = document.createElement('p');
+        p.textContent = question;
+        div.appendChild(p);
+        chatcontent.appendChild(div);
+        input.value = '';
+        response(question);
+    }
+})
+
+function response(input) {
+    let div = document.createElement('div');
+        div.setAttribute('class', 'chatresponse');
+        let p = document.createElement('p');
+        p.textContent = 'réponsetest';
+    let tail = document.createElement('span');
+    tail.setAttribute('class', 'tail');
+    div.appendChild(tail);
+        div.appendChild(p);
+        chatcontent.appendChild(div);
+}
